18b40beb75c945baf039f4c50e77ecdc3268451a
Sven <native2k@gmail.com>
2018-12-05 09:04:40 +0100

    extend getticker for product_code

--- bitflyer/public.py
+++ bitflyer/public.py
@@ -1,11 +1,11 @@
 import requests
 import simplejson as json
-
+from urllib.parse import urlencode, quote_plus
 """
 document: https://lightning.bitflyer.jp/docs
 """
 
-base_url = "https://api.bitflyer.jp/v1/"
+base_url = "https://api.bitflyer.com/v1/"
 api_urls = { 'getticker'     : '/getticker',
              'getexecutions' : '/getexecutions',
              'getboard'      : '/getboard',
@@ -17,26 +17,35 @@ class Public(object):
         pass
 
 
-    def public_api(self,url):
+    def public_api(self,url, **kwargs):
         ''' template function of public api'''
+        params = ''
+        if kwargs:
+            params = '?%s' % urlencode(kwargs, quote_via=quote_plus)
+
+        requrl = base_url + api_urls.get(url) + params
+
         try :
             url in api_urls
-            return json.loads(requests.get(base_url + api_urls.get(url)).text)
+            return json.loads(requests.get(requrl).text)
         except Exception as e:
-            print(e)
-    
-    def getticker(self):
+            print('ERROR request %s: %s' % (requrl, e))
+
+    def getticker(self, product_code=None):
         '''Ticker を取得'''
-        return self.public_api('getticker') 
-    
+        if product_code:
+            return self.public_api('getticker', product_code=product_code)
+        else:
+            return self.public_api('getticker')
+
     def getboard(self):
         ''' 板情報を取得 '''
-        return self.public_api('getboard') 
-    
+        return self.public_api('getboard')
+
     def getexecutions(self):
         ''' 約定の一覧を取得 '''
-        return self.public_api('getexecutions') 
+        return self.public_api('getexecutions')
 
     def gethealth(self):
         '''get exchange health'''
-        return self.public_api('gethealth') 
+        return self.public_api('gethealth')
